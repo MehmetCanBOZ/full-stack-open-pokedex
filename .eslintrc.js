85f22d87313f3085eb53dd4bb4aa05d0271a6a30
MehmetCanBOZ <bozmehmetcan01@gmail.com>
2023-05-28 15:58:26 +0300

    delete cypress env on eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,6 @@ module.exports = {
     browser: true,
     es6: true,
     node: true,
-    "cypress/globals": true,
   },
   extends: ["eslint:recommended", "plugin:react/recommended"],
   parserOptions: {
