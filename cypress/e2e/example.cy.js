ed67d852d53b4ed8fd77726ab30992110edcc380
MehmetCanBOZ <bozmehmetcan01@gmail.com>
2023-05-28 16:06:22 +0300

    fix test

--- cypress/e2e/example.cy.js
+++ cypress/e2e/example.cy.js
@@ -1,13 +1,13 @@
 describe("Pokedex", function () {
   it("front page can be opened", function () {
-    cy.visit("http://localhost:8080");
+    cy.visit("http://localhost:5000");
     cy.contains("ivysaur");
     cy.contains(
       "Pokémon and Pokémon character names are trademarks of Nintendo."
     );
   });
   it("detailed page can be opened", function () {
-    cy.visit("http://localhost:8080/pokemon/venusaur");
+    cy.visit("http://localhost:5000/pokemon/venusaur");
     cy.contains("venusaur");
     cy.contains("100");
   });
